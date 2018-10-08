442bed28485736ecd7f4d1b1ad3c0a66777d3657
Andrew Brook <flyingdutchcrab@gmail.com>
2018-10-08 12:46:21 -0400

    Built a basic hallway and got NewtonVR working, made a door with a UI control that makes it open and close

---
+++ Initial SteamVR Tests/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    // Public Variables
+    public bool isOpen;
+    // Private Variables
+    Animator anim;
+	// Use this for initialization
+	void Start ()
+    {
+        // Cache animator component
+        anim = GetComponent<Animator>();
+        // Set isOpen to be false
+        isOpen = false;
+	}
+	
+    public void Open()
+    {
+        // Check if isOpen is set to false
+        if (isOpen == false)
+        {
+            // Set isOpen to true
+            isOpen = true;
+            // Fire off the open trigger of the animator
+            anim.SetTrigger("Open");
+        }
+    }
+
+    public void Close()
+    {
+
+        // Check if isOpen is set to true
+        if (isOpen == true)
+        {
+            // Set isOpen to false
+            isOpen = false;
+            // Fire off the close trigger of the animator
+            anim.SetTrigger("Close");
+        }
+    }
+}
